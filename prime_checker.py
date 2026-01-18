5389d71d89438e39a5c0510ba45f57d865edc42b
Charmi87 <charmi.dholakia@intuitive.ai>
2026-01-18 23:29:54 +0530

    Create prime_checker.py with is_prime function

---
+++ prime_checker.py
@@ -0,0 +1,13 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    i = 3
+    while i * i <= number:
+        if number % i == 0:
+            return False
+        i += 2
+    return True
