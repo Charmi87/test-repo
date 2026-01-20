35008efbbaa41156d6b4a10a5a3bb1ad4a9468a9
Charmi87 <charmi.dholakia@intuitive.ai>
2026-01-20 05:59:09 +0530

    Add multiplyby7.py

---
+++ multiplyby7.py
@@ -0,0 +1,8 @@
+def multiply_by_7(number):
+    return number * 7
+
+if __name__ == "__main__":
+    # Example usage:
+    num = 5
+    result = multiply_by_7(num)
+    print(f"The result of multiplying {num} by 7 is: {result}")
